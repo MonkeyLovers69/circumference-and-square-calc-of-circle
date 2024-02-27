0ef852072db4e2bb9b4d71d872d1e3cf6faba4c7
MonkeyLovers69 <137757068+MonkeyLovers69@users.noreply.github.com>
2024-02-27 21:34:25 +0500

    Add files via upload

---
+++ dz-circle/dz-circle/dz-circle.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    string user_will;
+    cout << "do you want to find circle square/circumference? type 'yes' if you want to continue: ";
+    cin >> user_will;
+    while (user_will == "yes")
+    {
+        double Pi = 3.14;
+        int Radius;
+        cout << "Enter circle radius: ";
+        cin >> Radius;
+        cout << "Circle square is:" << pow(Radius, 2) * Pi << endl;
+        cout << "Circle circumference is: " << 2 * Pi * Radius << endl;
+        cout << "type 'yes' if you want to continue: ";
+        cin >> user_will;
+        system("cls");
+    }
+}
